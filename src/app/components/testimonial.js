90b41ede8ee7964efe4a5763e232ba134f4733f9
ribikaY <yonasribika@gmail.com>
2023-12-18 12:48:45 +0300

    Modified font size, padding and margin between the texts. Added a border to the images on 2 of the testimonial cards.

--- src/app/components/testimonial.js
+++ src/app/components/testimonial.js
@@ -3,7 +3,7 @@ import quote from "../../../public/images/bg-pattern-quotation.svg"
 
 const Testimonial = ({testimonial}) => {
     return (
-      <div key={testimonial.id} className={`relative rounded-[8px] px-[30px] py-[24px] shadow-xl shadow-slate-300 xl:h-custom-testimonial ${testimonial.id === 1 ? 'bg-primary1 text-[#FFE4FF] md:col-start-1 md:col-span-3 xl:col-start-1 xl:col-span-2 xl:w-custom-testimonial'
+      <div key={testimonial.id} className={`relative rounded-[8px] pl-[30px] pr-[37px] py-[22px] shadow-xl shadow-slate-300 xl:h-custom-testimonial ${testimonial.id === 1 ? 'bg-primary1 text-[#FFE4FF] md:col-start-1 md:col-span-3 xl:col-start-1 xl:col-span-2 xl:w-custom-testimonial'
         : testimonial.id === 2 ? 'bg-primary2 text-primary4'
         : testimonial.id === 3 ? 'bg-primary4 text-primary2 shadow-2xl shadow-[#bdcad9] md:col-start-1 md:row-start-3 xl:col-start-1 xl:row-start-2 xl:h-custom-testimonial-small'
         : testimonial.id === 4 ? 'bg-primary3 text-primary4 xl:h-custom-testimonial-small md:col-span-3 xl:row-start-2 xl:col-start-2 xl:col-span-2'
@@ -12,7 +12,7 @@ const Testimonial = ({testimonial}) => {
         {testimonial.id === 1 && <div className="absolute right-20 top-0 z-0"><Image src={quote} alt="quote" width="104px" height="102px"/></div>}
          <div className="flex">
             <div className="inline-block w-[32px] h-[32px] self-end">
-              <Image src={testimonial.profile.img} alt={testimonial.id} width="32px" height="32px" className={`border-2 border-transparent rounded-full ${testimonial.id === 1 ? '' : testimonial.id === 4 && 'border-primary1'}`}/>
+              <Image src={testimonial.profile.img} alt={testimonial.id} width="32px" height="32px" className={`border-2 border-transparent rounded-full ${testimonial.id === 1 ? 'border-[#A980EB]' : testimonial.id === 4 && 'border-[#7541C8]'}`}/>
             </div>
             <div className={`ml-[15px] `}>
               <p>
@@ -25,8 +25,8 @@ const Testimonial = ({testimonial}) => {
          </div>
          
         <div className={`relative z-10`}>
-          <h1 className={`font-semibold mt-[17px] text-[20px] leading-[1.55rem] ${testimonial.id === 1 ? 'text-[#FFE4FF]' : testimonial.id === 3 ? 'pr-1 text-primary2' : testimonial.id === 5 ? 'text-primary2': 'text-[#edf2f8]' }`}>{testimonial.title}</h1>
-          <p className={`leading-[1.15rem] opacity-70 ${testimonial.id === 3 || testimonial.id === 4 || testimonial.id === 5 ? 'mt-[20px]' : ' mt-4'}`}>&quot; {testimonial.text} &quot;</p>
+          <h1 className={`font-semibold mt-[16px] text-[19px] leading-[1.45rem] ${testimonial.id === 1 ? 'text-[#FFE4FF]' : testimonial.id === 3 ? 'pr-[10px] text-primary2' : testimonial.id === 5 ? 'text-primary2': 'text-[#edf2f8]' }`}>{testimonial.title}</h1>
+          <p className={`leading-[1.13rem] opacity-70 ${testimonial.id === 3 || testimonial.id === 4 || testimonial.id === 5 ? 'mt-[20px]' : ' mt-4'}`}>&quot; {testimonial.text} &quot;</p>
         </div>
        </div>
     )
